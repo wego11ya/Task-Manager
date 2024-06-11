40926b69cfeea8c086fd2d61942f1deb6ef057c8
wego11ya <wego11ya@gmail.com>
2024-06-11 16:42:13 +0800

    feat: initialize Mongoose connection

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const tasks = require("./routes/tasks");
+const connectDB = require("./db/connect");
 
 // middleware
 app.use(express.json());
@@ -14,4 +16,13 @@ app.use("/api/v1/tasks", tasks);
 
 const port = 3000;
 
-app.listen(port, console.log(`Server is listening on port ${port}`));
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGODB_URI);
+    app.listen(port, console.log(`Server is listening on port ${port}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
---
+++ db/connect.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const connectDB = (url) => {
+  return mongoose.connect(url).then(() => {
+    console.log("db connected");
+  });
+};
+
+module.exports = connectDB;
