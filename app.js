3937231bf0b8cc9832405139e1fe342a27a5c5a7
wego11ya <wego11ya@gmail.com>
2024-06-13 11:54:26 +0800

    feat: add error handling

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const tasks = require("./routes/tasks");
 const connectDB = require("./db/connect");
 const notFound = require("./middleware/not-found");
-
+const errorHandlerMiddleware = require("./middleware/error-handler");
 // middleware
 app.use(express.static("./public"));
 app.use(express.json());
@@ -13,6 +13,7 @@ app.use(express.json());
 
 app.use("/api/v1/tasks", tasks);
 app.use(notFound);
+app.use(errorHandlerMiddleware);
 
 const port = 3000;
 
